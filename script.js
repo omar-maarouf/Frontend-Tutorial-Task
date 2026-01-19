610ce4e366f9fdbea1d6f1d9b874eb174f487cb3
omar-maarouf <omar.maarouf@ejada.com>
2026-01-19 14:49:20 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+function changeNav(tab)
+{
+    var navLinks = document.getElementsByTagName("nav")[0];
+    for (var i = 0; i < navLinks.children.length; i++)
+    {
+        navLinks.children[i].classList.replace("active", "inactive");
+        navLinks.children[i].children[0].children[0].style.fill = "";
+    }
+    document.getElementById(tab).classList.replace("inactive", "active");
+}
+
+function buildCalendar()
+{
+    var days = document.getElementsByClassName("days")[0];
+    for (var i = 0; i < days.children.length; i++)
+    {
+        if(days.children[i].innerText == "15")
+        {
+            days.children[i].classList.add("active-day");
+        }
+        else
+        {
+            if(i == 0 || i == 1 || i > 32)
+            {
+                days.children[i].classList.add("inavailable-day");
+            }
+            else
+            {
+                days.children[i].classList.add("available-day");
+            }
+        }
+    }
+}
+
+function rightNav()
+{
+    var navLinks = document.getElementsByClassName("top-cards")[0].children[1];
+    var x = navLinks.children[0].clientWidth + parseInt(window.getComputedStyle(navLinks).gap) + 3;
+    navLinks.scrollBy((-1 * x), 0);
+}
+
+function leftNav()
+{
+    var navLinks = document.getElementsByClassName("top-cards")[0].children[1];
+    var x = navLinks.children[0].clientWidth + parseInt(window.getComputedStyle(navLinks).gap) + 3;
+    navLinks.scrollBy(x, 0);
+}
